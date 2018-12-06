1667a6fff55a2ea9f2a2ca40efdebe65eca236d0
avalonLZ <423810942@qq.com>
2018-12-06 01:19:22 -0800

    加入结构体嵌套union计算sizeof大小

---
+++ C_and_CPP/C/验证程序/test_union_struct_size.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: test_union_struct_size.c
+	> Author:lizhong
+	> Mail:423810942@qq.com
+	> Created Time: Thu 06 Dec 2018 12:18:34 AM PST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+struct test
+{
+    char t_i;
+    union{
+        char u_a;
+        int u_i[3];
+        double u_b;
+    };
+    int t_j;
+}test_t;
+
+int main(int argc, char *argv[])
+{
+    printf("sizeof:%d\n", sizeof(test_t));
+    return 0;
+}
