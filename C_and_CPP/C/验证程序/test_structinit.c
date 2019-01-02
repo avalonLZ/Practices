4c77973debe7b57bbccbdc4eb37f29a7bb0d39b4
avalonLZ <423810942@qq.com>
2019-01-02 00:28:15 -0800

    修改结构体初始化实验

--- C_and_CPP/C/验证程序/test_structinit.c
+++ C_and_CPP/C/验证程序/test_structinit.c
@@ -21,6 +21,10 @@ static struct Test test = {.name  = 'a', .age  = 12};
 
 int main(int argc, char *argv[])
 {
-    printf("name = %c\n\rage = %d", test.name, test.age);
+    //内核所采用的结构体初始化方式
+    struct Test mtest = {.name  = 'b', .age  = 24};
+    
+    printf("name = %c\n\rage = %d\n", test.name, test.age);
+    printf("name = %c\n\rage = %d\n", mtest.name, mtest.age);
     return 0;
 }
