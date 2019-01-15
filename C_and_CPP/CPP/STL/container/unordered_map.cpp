ea247629a4de96aaacfddae3d7b65f09e9f84490
avalonLZ <423810942@qq.com>
2019-01-14 17:29:48 -0800

    修改string类实验

---
+++ C_and_CPP/CPP/STL/container/unordered_map.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: unordered_map.cpp
+	> Author:lizhong
+	> Mail:423810942@qq.com
+	> Created Time:Sun 13 Jan 2019 09:24:58 PM PST
+	> Instruction:unordered_map使用
+ ************************************************************************/
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<unordered_map>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
--- C_and_CPP/CPP/验证程序/string.cpp
+++ C_and_CPP/CPP/验证程序/string.cpp
@@ -20,9 +20,26 @@ void string_test1(void)
     cout << s.c_str() << endl;
 }
 
+void string_test2(void)
+{
+    string s = "123abc";
+    cout << s.length() << endl;
+}
+
+void string_test3(void)
+{
+    cout << "****************test3**************" << endl;
+    string s = "cc1234";
+    
+    //推荐用substr方法截取字符串，而不使用string初始化的方式截取
+    string s1 = s.substr(0, 2);//string(s, 0, 2);
+    cout << "s1 = " << s1 << endl;
+}
+
 int main(int argc, char *argv[])
 {
     string_test1();
-
+    string_test2();
+    string_test3();
     return 0;
 }
