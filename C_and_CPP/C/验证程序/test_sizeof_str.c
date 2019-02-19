77438eae3bd141c572b77438f5efe173046c43fa
avalonLZ <423810942@qq.com>
2019-02-18 18:15:00 -0800

    修改柔性数组实验

---
+++ C_and_CPP/C/验证程序/test_sizeof_str.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: test_sizeof_str.c
+	> Author:lizhong
+	> Mail:423810942@qq.com
+	> Created Time:Mon 18 Feb 2019 05:55:37 PM PST
+	> Instruction:
+ ************************************************************************/
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(int argc, char *argv[])
+{
+    printf("sizeof(\"zhong\") is %d\n", sizeof("zhong"));
+    return 0;
+}
--- C_and_CPP/C/验证程序/test_softarray.c
+++ C_and_CPP/C/验证程序/test_softarray.c
@@ -30,17 +30,19 @@ int main(int argc, char *argv[])
     long int *pi;
    
     /*sizeof test*/
-    printf("before sizeof int is %d\r\n", sizeof(*pi));
-    printf("before sizeof test_tt is %d\r\n", sizeof(*t));
-    printf("before sizeof test_t is %d\r\n", sizeof(*pt));//1
+    printf("before sizeof int is %d\r\n", sizeof(*pi));//before sizeof int is 8
+    printf("before sizeof test_tt is %d\r\n", sizeof(*t));//before sizeof test_tt is 8
+    printf("before sizeof test_t is %d\r\n", sizeof(*pt));//before sizeof test_t is 8
    
     /*soft array test*/ 
     pt = malloc(sizeof(*pt) + sizeof("hello"));
     pt->tlen = sizeof("hello");
     strcpy(pt->t, "hello");
-    printf("after sizeof test_t is %d\r\n", sizeof(*pt));//还是1
-    printf("after pt->t:%s\r\n", pt->t);
-    
+    printf("after sizeof test_t is %d\r\n", sizeof(*pt));//after sizeof test_t is 8
+    printf("after pt->t:%s\r\n", pt->t);//after pt->t:hello
+   
+    free(pt);//t所占用的内存也将被释放，因为都是一malloc次分配出来的
+
     /*if t[1] is in test_t*/
     //printf("after pt->t size is %d\r\n", sizeof(pt->t)); 
     return 0;
