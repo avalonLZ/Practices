e8297f9e33d3904770afad2c6415fbb66a2c1418
avalonLZ <423810942@qq.com>
2019-09-07 11:57:49 +0800

    完善内存池_step1

--- DataStruct_Calculate/内存池/mempool.c
+++ DataStruct_Calculate/内存池/mempool.c
@@ -9,32 +9,61 @@
 #include<string.h>
 #include<stdlib.h>
 #include<math.h>
+#include<stdarg.h>
 
 typedef struct
 {
-    unsigned int type_num;
-    unsigned int type_size;
+    unsigned int block_num;
+    unsigned int block_size;
+    void *block;
 }mp_element_t;
 
 typedef struct
 {
-    mp_element *type;
+    mp_element_t *type;
+    unsigned int size;
 }mp_param_t;
 
-void mempool_param_fill()
+void mempool_mem_check()
 {
-    if(argv[1] == NULL)
-    {
-        exit(1);
-    }
+
 }
 
-void *mempool_init()
+void mempool_param_fill(mp_param_t *param, unsigned int type, ...)
 {
+    int i = 0;
+    param->size = type;
+    param->type = (mp_element_t *)malloc(sizeof(mp_element_t) * type);
+    
+    va_list val = {0};
+    va_start(val, type);
+    printf("val = %d\n", *val);
+
+    for(i = 0; i < type; ++i)
+    {
+        int tmp_num = 0;
+        int tmp_size = 0;
+        tmp_num = va_arg(val, int);
+        tmp_size = va_arg(val, int);
+
+        param->type[i].block_num = tmp_num;
+        param->type[i].block_size = tmp_size;
+        param->type[i].block = malloc(tmp_num * tmp_size);
+    }
+    va_end(val);
+}
+
+void *mempool_init(mp_param_t *param)
+{
+    mempool_param_fill(param, 2, 34, 4, 5, 6);
     return NULL;
 }
 
 int main(int argc, char *argv[])
 {
+    void *mempool = NULL;
+    mp_param_t mp_param = {0};
+
+    mempool = mempool_init(&mp_param);
     return 0;
 }
