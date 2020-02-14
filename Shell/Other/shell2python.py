a0ce5a99ccb321f4dac4469b6b8a073620e56f13
avalonLZ <423810942@qq.com>
2020-02-14 02:38:14 -0800

    添加shell调用python试验,并获取执行结果

--- Shell/Other/shell2python.py
+++ Shell/Other/shell2python.py
@@ -4,13 +4,15 @@
 from sys import exit
 
 def main(arg):
+        #shell中程序返回1
     if arg is False:
         exit(1)
+        #shell中程序返回0
     elif arg is True:
         exit(0)
     else:
-        #默认返回是0,执行成功
-        return
+        #shell中程序返回0
+        return 3
 
 if __name__ == '__main__':
     main(3)
