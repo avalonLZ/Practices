83992cc5030b8a071c1a431f568b689a332688cc
avalonLZ <423810942@qq.com>
2021-03-27 06:33:00 -0700

    添加结构体初始化测试

--- C_and_CPP/C/验证程序/struct_assignment.c
+++ C_and_CPP/C/验证程序/struct_assignment.c
@@ -9,9 +9,11 @@
 #include<string.h>
 #include<stdlib.h>
 
+#define MAX_SIZE 10
+
 typedef struct
 {
-    char strs[4];
+    char strs[MAX_SIZE];
     int i;
     unsigned char ii;
 
@@ -20,10 +22,16 @@ typedef struct
 
 int main(void)
 {
-    Test_t t1 = {0};
+    //直接填0可以将结构体中所有变量初值都至为0
+    //包括数组
+    Test_t t1;//= {0};
     t1.i = 100;
     Test_t t2;
+    int i = 0;
 
     t2 = t1;
     printf("%d\r\n", t2.i);
+
+    for (i = 0; i < MAX_SIZE; ++i)
+        printf("***i:%d*****%d*****\n", i, t2.strs[i]);
 }
